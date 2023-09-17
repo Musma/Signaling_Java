1e5708e7830d585f574a154c520a878affb91ab5
mac <shb@musma.net>
2023-09-17 13:21:28 +0900

    chore : ffmpeg 옵션 수정

--- src/main/resources/static/rtsp/stream.js
+++ src/main/resources/static/rtsp/stream.js
@@ -3,9 +3,10 @@ stream = new Stream({
 	name: 'name',
 	streamUrl: 'rtsp://admin:1234@192.168.0.101:554/stream0',
 	wsPort: 9999,
-	ffmpegOptions: { // options ffmpeg flags
-		'-stats': '', // an option with no neccessary value uses a blank string
-		'-r': 30 // options with required values specify the value after the key
+	ffmpegOptions: {
+		'-stats': '',
+		'-r': 30,
+		'-f': 'mpeg1video'
 	}
 })
 
